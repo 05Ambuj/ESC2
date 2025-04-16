bc4e821371d113c3dbc22518f581c868a287adfe
Ambuj Mishra <hyeambuj@gmail.com>
2025-04-16 17:35:57 +0530

    Updated changes

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -10,7 +10,7 @@ const UserPage = () => {
         <Link href="/">
           <Image path="icons/back.svg" alt="back" w={24} h={24} />
         </Link>
-        <h1 className="font-bold text-lg">Lama Dev</h1>
+        <h1 className="font-bold text-lg">Dmitri</h1>
       </div>
       {/* INFO */}
       <div className="">
@@ -43,10 +43,10 @@ const UserPage = () => {
         <div className="p-4 flex flex-col gap-2">
           {/* USERNAME & HANDLE */}
           <div className="">
-            <h1 className="text-2xl font-bold">Lama Dev</h1>
-            <span className="text-textGray text-sm">@lamaWebDev</span>
+            <h1 className="text-2xl font-bold">Dmitri</h1>
+            <span className="text-textGray text-sm">@Dmitri</span>
           </div>
-          <p>Lama Dev Youtube Channel</p>
+          <p>Escapo Developer</p>
           {/* JOB & LOCATION & DATE */}
           <div className="flex gap-4 text-textGray text-[15px]">
             <div className="flex items-center gap-2">
@@ -56,21 +56,21 @@ const UserPage = () => {
                 w={20}
                 h={20}
               />
-              <span>USA</span>
+              <span>India</span>
             </div>
             <div className="flex items-center gap-2">
               <Image path="icons/date.svg" alt="date" w={20} h={20} />
-              <span>Joined May 2021</span>
+              <span>Joined March 2025</span>
             </div>
           </div>
           {/* FOLLOWINGS & FOLLOWERS */}
           <div className="flex gap-4">
             <div className="flex items-center gap-2">
-              <span className="font-bold">100</span>
+              <span className="font-bold">1</span>
               <span className="text-textGray text-[15px]">Followers</span>
             </div>
             <div className="flex items-center gap-2">
-              <span className="font-bold">100</span>
+              <span className="font-bold">101</span>
               <span className="text-textGray text-[15px]">Followings</span>
             </div>
           </div>
--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -45,12 +45,6 @@ const menuList = [
     icon: "community.svg",
   },
   {
-    id: 8,
-    name: "Premium",
-    link: "/",
-    icon: "logo.svg",
-  },
-  {
     id: 9,
     name: "Profile",
     link: "/",
@@ -71,10 +65,10 @@ const LeftBar = () => {
       <div className="flex flex-col gap-4 text-lg items-center xxl:items-start">
         {/* LOGO */}
         <Link href="/" className="p-2 rounded-full hover:bg-[#181818] ">
-          <Image path="icons/logo.svg" alt="logo" w={24} h={24} />
+          <Image path="icons/AppLogo.svg" alt="logo" w={24} h={24} />
         </Link>
         {/* MENU LIST */}
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-3">
           {menuList.map((item) => (
             <Link
               href={item.link}
@@ -109,11 +103,11 @@ const LeftBar = () => {
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-2">
           <div className="w-10 h-10 relative rounded-full overflow-hidden">
-            <Image path="/general/avatar.png" alt="lama dev" w={100} h={100} tr={true} />
+            <Image path="/general/avatar.png" alt="user" w={100} h={100} tr={true} />
           </div>
           <div className="hidden xxl:flex flex-col">
-            <span className="font-bold">Lama Dev</span>
-            <span className="text-sm text-textGray">@lamaWebDev</span>
+            <span className="font-bold">Dmitri</span>
+            <span className="text-sm text-textGray">@Dmitri</span>
           </div>
         </div>
         <div className="hidden xxl:block cursor-pointer font-bold">...</div>
